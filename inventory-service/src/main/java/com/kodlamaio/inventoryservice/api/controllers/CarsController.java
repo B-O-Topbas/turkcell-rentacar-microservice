8f306ed974686ab156e1f1b794864de1fa517e1c
B-O-Topbas <onurbeytullah58@hotmail.com>
2023-06-04 17:06:43 +0300

    common package artik central de

--- inventory-service/src/main/java/com/kodlamaio/inventoryservice/api/controllers/CarsController.java
+++ inventory-service/src/main/java/com/kodlamaio/inventoryservice/api/controllers/CarsController.java
@@ -11,6 +11,7 @@ import com.kodlamaio.inventoryservice.business.dto.responses.update.UpdateCarRes
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +24,7 @@ public class CarsController {
     private final CarService service;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('user','admin')")
     public List<GetAllCarsResponse> getAll() {
         return service.getAll();
     }
